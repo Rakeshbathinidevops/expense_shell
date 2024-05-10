182f01af965f7e9e53ec7fa7ff349643e7136991
rakeshbathinidevops <rakeshbp2017@gmail.com>
2024-05-10 15:17:59 +0530

    message

--- backend.sh
+++ backend.sh
@@ -57,7 +57,7 @@ VALIDATE $? "zip file unzipping"
 npm install 2>>$LOGFILE
 VALIDATE $? "nodejs dependecies installing"
 
-cp /home/ec2-user/expense-shell/backend.service /etc/systemd/system/backend.service &>>$LOGFILE
+cp /home/ec2-user/expense_shell/backend.service /etc/systemd/system/backend.service &>>$LOGFILE
 VALIDATE $? "Copied backend service"
 
 systemctl daemon-reload &>>$LOGFILE
